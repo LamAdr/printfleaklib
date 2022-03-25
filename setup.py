482721bd65a48d2901e7d16a249dc9518f8ddcea
LamAdr <102169875+LamAdr@users.noreply.github.com>
2022-03-25 08:45:20 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='printfleaklib',
-    packages=find_packages(include=['printfleaklib']),
+    packages=['printfleaklib'],
     description='Manages printf format string leaks',
     install_requires=['pwn', 'prettytable'],
 )
